fdf57ccd80c69b8cd2c694d08dd73a7c87c9fc93
Adam Johnson <adam@chromative.net>
2020-09-26 20:17:50 -0500

    Update Program.fs

--- FSharpFunctionSandboxExecute/Program.fs
+++ FSharpFunctionSandboxExecute/Program.fs
@@ -2,11 +2,10 @@
 
 open System
 open FSharpFunctions
-open UnitsUOM.SI.UnitNames
 
 
 [<EntryPoint>]
 let main argv =
-    let temperature = FSharpFunctions.FSharpFunctions.VolumeConversion.convertVolume(1.00000033435325741461344m,"milliliters","minimsUk")
+    let temperature = FSharpFunctions.VolumeConversion.convertVolume(1.00000033435325741461344m,"milliliters","minimsUk")
     printfn "%A" temperature
     0 // return an integer exit code
